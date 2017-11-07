901917c37f01f253e01d525706c9a87684d864aa
Oli Slade <oliver.slade@digital.hmrc.gov.uk>
2017-11-07 13:19:12 +0000

    Updating g8 scaffolds to latest versions

--- .g8/repeater/app/uk/gov/hmrc/socialcarecompliancefrontend/viewmodels/$className$OverviewViewModel.scala
+++
@@ -1,6 +0,0 @@
-package viewmodels
-
-import play.api.mvc.Call
-import models.$className$
-
-case class $className$OverviewViewModel($pluralName;format="decap"$: Seq[$className$], add$className$Route: Call, nextPage: Call)
