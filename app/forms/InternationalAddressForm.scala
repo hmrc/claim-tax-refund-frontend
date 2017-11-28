02ac54cb52dbb348d6ddc7586eb1b40135d5b889
Oli Slade <oliver.slade@digital.hmrc.gov.uk>
2017-11-28 12:29:08 +0000

    Refactoring International address to use constraints

--- app/forms/InternationalAddressForm.scala
+++ app/forms/InternationalAddressForm.scala
@@ -22,20 +22,31 @@ import play.api.data.Form
 import play.api.data.Forms._
 import models.InternationalAddress
 
-class InternationalAddressForm @Inject() (appConfig: FrontendAppConfig) extends FormErrorHelper with FormatterMaxLength{
+class InternationalAddressForm @Inject() (appConfig: FrontendAppConfig) extends FormErrorHelper with Constraints {
 
-  private val maxLength = appConfig.addressLineMaxLength
+  private val maxLengthInt = appConfig.addressLineMaxLength
   private val countryLength = appConfig.countryMaxLength
+
+  private val addressLine1KeyBlank = "global.addressLine1.blank"
+  private val addressLine1KeyTooLong = "global.addressLine1.tooLong"
+  private val addressLine2KeyBlank = "global.addressLine2.blank"
+  private val addressLine2KeyTooLong = "global.addressLine2.tooLong"
+  private val addressLine3KeyTooLong = "global.addressLine3.tooLong"
+  private val addressLine4KeyTooLong = "global.addressLine4.tooLong"
+  private val addressLine5KeyTooLong = "global.addressLine5.tooLong"
+  private val countryKeyBlank = "global.country.blank"
+  private val countryKeyTooLong = "global.country.tooLong"
 
   def apply(): Form[InternationalAddress] = {
     Form(
       mapping(
-        "addressLine1" -> of(formatterMaxLength("global.addressLine1", maxLength)),
-        "addressLine2" -> of(formatterMaxLength("global.addressLine2", maxLength)),
-        "addressLine3" -> optional(of(formatterMaxLength("global.addressLine3", maxLength))),
-        "addressLine4" -> optional(of(formatterMaxLength("global.addressLine4", maxLength))),
-        "addressLine5" -> optional(of(formatterMaxLength("global.addressLine5", maxLength))),
-        "country" -> of(formatterMaxLength("global.country", countryLength))
-      )(InternationalAddress.apply)(InternationalAddress.unapply))
+        "addressLine1" -> text.verifying(nonEmpty(addressLine1KeyBlank), maxLength(maxLengthInt, addressLine1KeyTooLong)),
+        "addressLine2" -> text.verifying(nonEmpty(addressLine2KeyBlank), maxLength(maxLengthInt, addressLine2KeyTooLong)),
+        "addressLine3" -> optional(text.verifying(maxLength(maxLengthInt, addressLine3KeyTooLong))),
+        "addressLine4" -> optional(text.verifying(maxLength(maxLengthInt, addressLine4KeyTooLong))),
+        "addressLine5" -> optional(text.verifying(maxLength(maxLengthInt, addressLine5KeyTooLong))),
+        "country"      -> text.verifying(nonEmpty(countryKeyBlank), maxLength(countryLength, countryKeyTooLong))
+      )(InternationalAddress.apply)(InternationalAddress.unapply)
+    )
   }
 }
