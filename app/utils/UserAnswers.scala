d548fa43f9bbbc38bedd34322a0c3fd733a64337
Thomas Pascoe <pascoe121@gmail.com>
2017-12-04 11:22:21 +0000

    create UTR page

--- app/utils/UserAnswers.scala
+++ app/utils/UserAnswers.scala
@@ -21,6 +21,8 @@ import identifiers._
 import models._
 
 class UserAnswers(val cacheMap: CacheMap) {
+  def uniqueTaxpayerReference: Option[String] = cacheMap.getEntry[String](UniqueTaxpayerReferenceId.toString)
+
   def areYouSelfAssessed: Option[Boolean] = cacheMap.getEntry[Boolean](AreYouSelfAssessedId.toString)
 
   def telephoneNumber: Option[String] = cacheMap.getEntry[String](TelephoneNumberId.toString)
