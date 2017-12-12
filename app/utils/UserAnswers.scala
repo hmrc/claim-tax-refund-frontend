813aec2a3ab355ead1135503ec5c1dcbfbe5d598
Thomas Pascoe <pascoe121@gmail.com>
2017-12-12 09:04:20 +0000

    create how much bank or building society interest page

--- app/utils/UserAnswers.scala
+++ app/utils/UserAnswers.scala
@@ -21,6 +21,8 @@ import identifiers._
 import models._
 
 class UserAnswers(val cacheMap: CacheMap) {
+  def howMuchBankBuildingSocietyInterest: Option[String] = cacheMap.getEntry[String](HowMuchBankBuildingSocietyInterestId.toString)
+
   def anyBankBuildingSocietyInterest: Option[Boolean] = cacheMap.getEntry[Boolean](AnyBankBuildingSocietyInterestId.toString)
 
   def howMuchRentalIncome: Option[String] = cacheMap.getEntry[String](HowMuchRentalIncomeId.toString)
