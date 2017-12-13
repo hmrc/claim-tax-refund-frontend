4cd435903e53313d3e8968c79e7f63d8a4af141c
Matthew Beck <matthew.beck@digital.hmrc.gov.uk>
2017-12-13 08:29:55 +0000

    Other Benefits Details and Amount scaffold

--- app/utils/UserAnswers.scala
+++ app/utils/UserAnswers.scala
@@ -21,6 +21,8 @@ import identifiers._
 import models._
 
 class UserAnswers(val cacheMap: CacheMap) {
+  def otherBenefitsDetailsAndAmount: Option[String] = cacheMap.getEntry[String](OtherBenefitsDetailsAndAmountId.toString)
+
   def anyOtherTaxableIncome: Option[Boolean] = cacheMap.getEntry[Boolean](AnyOtherTaxableIncomeId.toString)
 
   def howMuchStatePension: Option[String] = cacheMap.getEntry[String](HowMuchStatePensionId.toString)
