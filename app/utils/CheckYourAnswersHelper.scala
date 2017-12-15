0d5cad40fa27eb59e2c3bbf2ce7a8b4456b272df
warren smithson <warren.smithson@digital.hmrc.gov.uk>
2017-12-15 14:21:11 +0000

    Created Do they have an agent ref page

--- app/utils/CheckYourAnswersHelper.scala
+++ app/utils/CheckYourAnswersHelper.scala
@@ -22,6 +22,10 @@ import viewmodels.{AnswerRow, RepeaterAnswerRow, RepeaterAnswerSection}
 
 class CheckYourAnswersHelper(userAnswers: UserAnswers) {
 
+  def anyAgentRef: Option[AnswerRow] = userAnswers.anyAgentRef map {
+    x => AnswerRow("anyAgentRef.checkYourAnswersLabel", if(x) "site.yes" else "site.no", true, routes.AnyAgentRefController.onPageLoad(CheckMode).url)
+  }
+
   def agentReferenceNumber: Option[AnswerRow] = userAnswers.agentReferenceNumber map {
     x => AnswerRow("agentReferenceNumber.checkYourAnswersLabel", s"$x", false, routes.AgentReferenceNumberController.onPageLoad(CheckMode).url)
   }
