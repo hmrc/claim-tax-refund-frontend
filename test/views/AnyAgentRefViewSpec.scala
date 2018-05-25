2e5a277c8d738a1a9f8cd26a65b53e469d8e322e
Thomas Pascoe <pascoe121@gmail.com>
2018-05-25 13:02:02 +0100

    update any agent ref tests

--- test/views/AnyAgentRefViewSpec.scala
+++ test/views/AnyAgentRefViewSpec.scala
@@ -17,17 +17,19 @@
 package views
 
 import controllers.routes
-import forms.BooleanForm
-import models.NormalMode
-import play.api.data.Form
-import views.behaviours.YesNoViewBehaviours
+import forms.AnyAgentReferenceForm
+import models.{AgentRef, NormalMode}
+import play.api.data.{Form, FormError}
+import play.twirl.api.HtmlFormat
+import views.behaviours.QuestionViewBehaviours
 import views.html.anyAgentRef
 
-class AnyAgentRefViewSpec extends YesNoViewBehaviours {
+class AnyAgentRefViewSpec extends QuestionViewBehaviours[AgentRef]{
 
   val messageKeyPrefix = "anyAgentRef"
 
-  override val form = new BooleanForm()()
+  val formProvider = new AnyAgentReferenceForm()
+  val form = formProvider()
 
   def createView = () => anyAgentRef(frontendAppConfig, form, NormalMode)(fakeRequest, messages)
 
@@ -39,6 +41,80 @@ class AnyAgentRefViewSpec extends YesNoViewBehaviours {
 
     behave like pageWithBackLink(createView)
 
-    behave like yesNoPage(createViewUsingForm, messageKeyPrefix, routes.AnyAgentRefController.onSubmit(NormalMode).url)
+    yesNoPage(createViewUsingForm, messageKeyPrefix, routes.AnyAgentRefController.onSubmit(NormalMode).url)
+
+    def yesNoPage(createView: (Form[AgentRef]) => HtmlFormat.Appendable,
+                  messageKeyPrefix: String,
+                  expectedFormAction: String,
+                  expectedHintText: Option[String] = None) = {
+
+      "behave like a page with a Yes/No question" when {
+        "rendered" must {
+          "contain a legend for the question" in {
+            val doc = asDocument(createView(form))
+            val legends = doc.getElementsByTag("legend")
+            legends.size mustBe 1
+          }
+
+          "contain a heading" in {
+            val doc = asDocument(createView(form))
+            assertContainsText(doc, messages(s"$messageKeyPrefix.heading"))
+          }
+
+          "contain an input for the value" in {
+            val doc = asDocument(createView(form))
+            assertRenderedById(doc, "agentRef.anyAgentRef-yes")
+            assertRenderedById(doc, "agentRef.anyAgentRef-no")
+          }
+
+          "have no values checked when rendered with no form" in {
+            val doc = asDocument(createView(form))
+            assert(!doc.getElementById("agentRef.anyAgentRef-yes").hasAttr("checked"))
+            assert(!doc.getElementById("agentRef.anyAgentRef-no").hasAttr("checked"))
+          }
+
+          "not render an error summary" in {
+            val doc = asDocument(createView(form))
+            assertNotRenderedById(doc, "error-summary_header")
+          }
+        }
+
+        "rendered with a value of true" must {
+          behave like answeredYesNoPage(createView, true)
+        }
+
+        "rendered with a value of false" must {
+          behave like answeredYesNoPage(createView, false)
+        }
+
+        "rendered with an error" must {
+          "show an error summary" in {
+            val doc = asDocument(createView(form.withError(error)))
+            assertRenderedById(doc, "error-summary-heading")
+          }
+
+          "show an error in the value field's label" in {
+            val doc = asDocument(createView(form.withError(FormError("agentRef.anyAgentRef", "Please enter a valid number"))))
+            val errorSpan = doc.getElementsByClass("error-notification").first
+            errorSpan.text mustBe messages(errorMessage)
+          }
+        }
+      }
+    }
+
+
+    def answeredYesNoPage(createView: (Form[AgentRef]) => HtmlFormat.Appendable, answer: Boolean) = {
+
+      "have only the correct value checked" in {
+        val doc = asDocument(createView(form.fill(AgentRef.Yes("AB1234"))))
+        assert(doc.getElementById("agentRef.anyAgentRef-yes").hasAttr("checked"))
+        assert(!doc.getElementById("agentRef.anyAgentRef-no").hasAttr("checked"))
+      }
+
+      "not render an error summary" in {
+        val doc = asDocument(createView(form.fill(AgentRef.Yes("AB1234"))))
+        assertNotRenderedById(doc, "error-summary_header")
+      }
+    }
   }
 }
