38953f1338a128e1edd65d9478984098c10dc1e0
Thomas Pascoe <pascoe121@gmail.com>
2018-06-04 10:00:13 +0100

    add dynamic header and update all tests

--- test/controllers/OtherCompanyBenefitsDetailsControllerSpec.scala
+++ test/controllers/OtherCompanyBenefitsDetailsControllerSpec.scala
@@ -16,53 +16,52 @@
 
 package controllers
 
-import play.api.data.Form
-import play.api.libs.json.JsString
-import uk.gov.hmrc.http.cache.client.CacheMap
-import utils.FakeNavigator
 import connectors.FakeDataCacheConnector
 import controllers.actions._
-import play.api.test.Helpers._
 import forms.OtherCompanyBenefitsDetailsForm
-import identifiers.OtherCompanyBenefitsDetailsId
 import models.NormalMode
+import models.SelectTaxYear.CYMinus2
+import org.scalatest.mockito.MockitoSugar
+import play.api.data.Form
+import play.api.test.Helpers._
+import utils.{FakeNavigator, MockUserAnswers}
 import views.html.otherCompanyBenefitsDetails
+import org.mockito.Mockito.when
 
-class OtherCompanyBenefitsDetailsControllerSpec extends ControllerSpecBase {
+class OtherCompanyBenefitsDetailsControllerSpec extends ControllerSpecBase with MockitoSugar {
 
   def onwardRoute = routes.IndexController.onPageLoad()
+
+  val testAnswer = "answer"
+  val form = new OtherCompanyBenefitsDetailsForm(frontendAppConfig)()
+  val taxYear: String = CYMinus2.asString
+  private val mockUserAnswers = MockUserAnswers.yourDetailsUserAnswers
 
   def controller(dataRetrievalAction: DataRetrievalAction = getEmptyCacheMap) =
     new OtherCompanyBenefitsDetailsController(frontendAppConfig, messagesApi, FakeDataCacheConnector, new FakeNavigator(desiredRoute = onwardRoute), FakeAuthAction,
       dataRetrievalAction, new DataRequiredActionImpl, new OtherCompanyBenefitsDetailsForm(frontendAppConfig))
 
-  val testAnswer = "answer"
-  val form = new OtherCompanyBenefitsDetailsForm(frontendAppConfig)()
-
-  def viewAsString(form: Form[_] = form) = otherCompanyBenefitsDetails(frontendAppConfig, form, NormalMode)(fakeRequest, messages).toString
+  def viewAsString(form: Form[_] = form) = otherCompanyBenefitsDetails(frontendAppConfig, form, NormalMode, taxYear)(fakeRequest, messages).toString
 
   "OtherCompanyBenefitsDetails Controller" must {
 
     "return OK and the correct view for a GET" in {
-      val result = controller().onPageLoad(NormalMode)(fakeRequest)
+      val result = controller(fakeDataRetrievalAction()).onPageLoad(NormalMode)(fakeRequest)
 
       status(result) mustBe OK
       contentAsString(result) mustBe viewAsString()
     }
 
     "populate the view correctly on a GET when the question has previously been answered" in {
-      val validData = Map(OtherCompanyBenefitsDetailsId.toString -> JsString(testAnswer))
-      val getRelevantData = new FakeDataRetrievalAction(Some(CacheMap(cacheMapId, validData)))
-
-      val result = controller(getRelevantData).onPageLoad(NormalMode)(fakeRequest)
+      when(mockUserAnswers.otherCompanyBenefitsDetails).thenReturn(Some(testAnswer))
+      val result = controller(fakeDataRetrievalAction(mockUserAnswers)).onPageLoad(NormalMode)(fakeRequest)
 
       contentAsString(result) mustBe viewAsString(form.fill(testAnswer))
     }
 
     "redirect to the next page when valid data is submitted" in {
       val postRequest = fakeRequest.withFormUrlEncodedBody(("value", testAnswer))
-
-      val result = controller().onSubmit(NormalMode)(postRequest)
+      val result = controller(fakeDataRetrievalAction()).onSubmit(NormalMode)(postRequest)
 
       status(result) mustBe SEE_OTHER
       redirectLocation(result) mustBe Some(onwardRoute.url)
@@ -72,7 +71,7 @@ class OtherCompanyBenefitsDetailsControllerSpec extends ControllerSpecBase {
       val postRequest = fakeRequest.withFormUrlEncodedBody(("value", ""))
       val boundForm = form.bind(Map("value" -> ""))
 
-      val result = controller().onSubmit(NormalMode)(postRequest)
+      val result = controller(fakeDataRetrievalAction()).onSubmit(NormalMode)(postRequest)
 
       status(result) mustBe BAD_REQUEST
       contentAsString(result) mustBe viewAsString(boundForm)
