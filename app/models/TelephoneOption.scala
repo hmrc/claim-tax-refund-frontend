b9a0cede808a89497d2607e6780330e68130119c
Thomas Pascoe <pascoe121@gmail.com>
2018-06-20 11:58:40 +0100

    create telephoneOption model

---
+++ app/models/TelephoneOption.scala
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2018 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package models
+
+import play.api.libs.json._
+
+sealed trait TelephoneOption
+
+object TelephoneOption {
+
+  case class Yes(number: String) extends TelephoneOption
+  case object No extends TelephoneOption
+
+  implicit val reads: Reads[TelephoneOption] = {
+    (JsPath \ "anyTelephone").read[Boolean].flatMap {
+      case true =>
+        (JsPath \ "telephoneNumber").read[String]
+          .map[TelephoneOption](Yes.apply)
+          .orElse(Reads[TelephoneOption](_ => JsError("TelephoneOption value expected")))
+      case false =>
+        Reads.pure(No)
+    }
+  }
+
+  implicit lazy val writes = new Writes[TelephoneOption] {
+    def writes(o: TelephoneOption): JsObject = {
+      o match {
+        case Yes(number) =>
+          Json.obj("anyTelephone" -> true, "telephoneNumber" -> number)
+        case _ =>
+          Json.obj("anyTelephone" ->false)
+      }
+    }
+  }
+
+}
