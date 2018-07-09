d48359b46dd85156256fc37d5e5cda3e7c89973c
Gareth Gooch <31101509+garethgooch@users.noreply.github.com>
2018-07-09 12:53:46 +0100

    Update WhereToSendPayment.scala

--- app/models/WhereToSendPayment.scala
+++ app/models/WhereToSendPayment.scala
@@ -16,9 +16,6 @@
 
 package models
 
-import play.api.libs.json._
-import utils.{EnumUtils, Enumerable, WithName}
-
 sealed trait WhereToSendPayment
 
 object WhereToSendPayment extends Enumerable[WhereToSendPayment] {
@@ -30,6 +27,4 @@ object WhereToSendPayment extends Enumerable[WhereToSendPayment] {
   lazy val values: Set[WhereToSendPayment] = Set(
     Myself, Nominee
   )
-
-
 }
