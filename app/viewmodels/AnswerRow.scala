526e9d015095265498a9a897233a47c4d62fc49e
Matthew Beck <matthew.beck@digital.hmrc.gov.uk>
2018-08-03 10:15:23 +0100

    CTR-254: Created remove link for CYA

--- app/viewmodels/AnswerRow.scala
+++ app/viewmodels/AnswerRow.scala
@@ -32,10 +32,10 @@ object Message {
     Message(str)
 }
 
-case class AnswerRow(label: Message, answer: Message, url: String)
+case class AnswerRow(label: Message, answer: Message, url: String, deleteLink: Boolean)
 
 object AnswerRow {
 
-  def apply(label: String, answer: String, answerIsMessageKey: Boolean, url: String): AnswerRow =
-    AnswerRow(Message(label), Message(answer), url)
+  def apply(label: String, answer: String, answerIsMessageKey: Boolean, url: String, deleteLink: Boolean = false): AnswerRow =
+    AnswerRow(Message(label), Message(answer), url, deleteLink)
 }
