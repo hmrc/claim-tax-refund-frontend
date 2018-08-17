c311c1b1848dfcff68cee405fe96c0b89acbaf2f
Matthew Beck <matthew.beck@digital.hmrc.gov.uk>
2018-08-17 09:48:17 +0100

    CTR-98: Refactor RobotSpec tests

--- test/models/templates/RobotsSpec.scala
+++ test/models/templates/RobotsSpec.scala
@@ -17,69 +17,71 @@
 package models.templates
 
 import base.SpecBase
-import models.WhereToSendPayment
+import models.UkAddress
 import models.templates.xml.robots
 import org.scalatest.mockito.MockitoSugar
-import play.api.Logger
 import play.twirl.api.XmlFormat
 import utils.{MockUserAnswers, RobotsXmlHelper, UserAnswers, WireMockHelper}
+import org.mockito.Mockito.when
 
 import scala.xml.XML._
 import scala.xml._
 
 class RobotsSpec extends  SpecBase with WireMockHelper with MockitoSugar {
+
   val fullUserAnswers: UserAnswers = MockUserAnswers.fullValidUserAnswers
   val fullXml: XmlFormat.Appendable = robots(fullUserAnswers, new RobotsXmlHelper(fullUserAnswers)(messages))
   val xmlToNode: Elem = loadString(fullXml.toString.replaceAll("\\s", "")) // TODO this needs to be removed once XML refactor is complete (remove whitespace)
+
+  def getXpath(elementSectionKey: String, elementKey: String, xmlToNode: Elem = xmlToNode): Node = {
+
+      val nodeSeq = xmlToNode \\ elementSectionKey
+      nodeSeq.\(elementKey).head
+  }
 
   "robots Xml" must {
 
     "have correct sections in userDetails" in {
 
-      val nodeSeq: NodeSeq = xmlToNode \\ "userDetails"
-
-      val name: NodeSeq = nodeSeq.\("name").head
-      val nino: NodeSeq = nodeSeq.\("nino").head
-
-      name mustBe <name>TestName</name>
-      nino mustBe <nino>ZZ123456A</nino>
+      getXpath("userDetails", "name") mustBe <name>TestName</name>
+      getXpath("userDetails", "nino") mustBe <nino>ZZ123456A</nino>
     }
-
-    //TODO make false version of this (with PAYE reference)
 
     "have correct sections in claimSection when employmentDetails are true" in {
 
-      val nodeSeq: NodeSeq = xmlToNode \\ "claimSection"
+      getXpath("claimSection", "selectedTaxYear") mustBe <selectedTaxYear>6April2016to5April2017</selectedTaxYear>
+      getXpath("claimSection", "employmentDetails") mustBe <employmentDetails>true</employmentDetails>
+    }
 
-      val selectedTaxYear: Node = nodeSeq.\("selectedTaxYear").head
-      val employmentDetails: Node = nodeSeq.\("employmentDetails").head
+    "have correct sections in claimSection when employmentDetails are false" in {
+      val fullUserAnswers: UserAnswers = MockUserAnswers.fullValidUserAnswers
 
-      selectedTaxYear mustBe <selectedTaxYear>6April2016to5April2017</selectedTaxYear>
-      employmentDetails mustBe <employmentDetails>true</employmentDetails>
+      when(fullUserAnswers.employmentDetails) thenReturn Some(false)
+      when(fullUserAnswers.enterPayeReference) thenReturn Some("123456789")
+      when(fullUserAnswers.detailsOfEmploymentOrPension) thenReturn Some("Employment details")
+
+      val fullXml: XmlFormat.Appendable = robots(fullUserAnswers, new RobotsXmlHelper(fullUserAnswers)(messages))
+      val newXmlToNode: Elem = loadString(fullXml.toString.replaceAll("\\s", ""))
+
+      getXpath("claimSection", "selectedTaxYear", newXmlToNode) mustBe <selectedTaxYear>6April2016to5April2017</selectedTaxYear>
+      getXpath("claimSection", "employmentDetails", newXmlToNode) mustBe <employmentDetails>false</employmentDetails>
+      getXpath("claimSection", "payeReference", newXmlToNode) mustBe <payeReference>123456789</payeReference>
+      getXpath("claimSection", "detailsOfEmploymentOrPension", newXmlToNode) mustBe <detailsOfEmploymentOrPension>Employmentdetails</detailsOfEmploymentOrPension>
+
     }
 
     "have correct sections in benefitSection" in {
 
-      val nodeSeq: NodeSeq = xmlToNode \\ "benefitSection"
+      getXpath("benefitSection", "anyBenefits") mustBe <anyBenefits>true</anyBenefits>
+      getXpath("benefitSection" ,"selectBenefits") mustBe <selectBenefits>carers-allowancebereavement-allowanceincapacity-benefitemployment-and-support-allowancejobseekers-allowanceother-taxable-benefitstate-pension</selectBenefits>
+      getXpath("benefitSection", "howMuchBereavementAllowance") mustBe <howMuchBereavementAllowance>1234</howMuchBereavementAllowance>
+      getXpath("benefitSection", "howMuchCarersAllowance") mustBe <howMuchCarersAllowance>1234</howMuchCarersAllowance>
+      getXpath("benefitSection", "howMuchJobseekersAllowance") mustBe <howMuchJobseekersAllowance>1234</howMuchJobseekersAllowance>
+      getXpath("benefitSection", "howMuchEmploymentAndSupportAllowance") mustBe <howMuchEmploymentAndSupportAllowance>1234</howMuchEmploymentAndSupportAllowance>
+      getXpath("benefitSection", "howMuchIncapacityBenefit") mustBe <howMuchIncapacityBenefit>1234</howMuchIncapacityBenefit>
+      getXpath("benefitSection", "howMuchStatePension") mustBe <howMuchStatePension>1234</howMuchStatePension>
 
-      val anyBenefits = nodeSeq.\("anyBenefits").head
-      val selectBenefits = nodeSeq.\("selectBenefits").head
-      val howMuchBereavementAllowance = nodeSeq.\("howMuchBereavementAllowance").head
-      val howMuchCarersAllowance = nodeSeq.\("howMuchCarersAllowance").head
-      val howMuchJobseekersAllowance = nodeSeq.\("howMuchJobseekersAllowance").head
-      val howMuchEmploymentAndSupportAllowance = nodeSeq.\("howMuchEmploymentAndSupportAllowance").head
-      val howMuchIncapacityBenefit = nodeSeq.\("howMuchIncapacityBenefit").head
-      val howMuchStatePension = nodeSeq.\("howMuchStatePension").head
-      val otherBenefits = nodeSeq.\("otherBenefitsSection").\("otherTaxableIncome").\("otherBenefit")
-
-      anyBenefits mustBe <anyBenefits>true</anyBenefits>
-      selectBenefits mustBe <selectBenefits>carers-allowancebereavement-allowanceincapacity-benefitemployment-and-support-allowancejobseekers-allowanceother-taxable-benefitstate-pension</selectBenefits>
-      howMuchBereavementAllowance mustBe <howMuchBereavementAllowance>1234</howMuchBereavementAllowance>
-      howMuchCarersAllowance mustBe <howMuchCarersAllowance>1234</howMuchCarersAllowance>
-      howMuchJobseekersAllowance mustBe <howMuchJobseekersAllowance>1234</howMuchJobseekersAllowance>
-      howMuchEmploymentAndSupportAllowance mustBe <howMuchEmploymentAndSupportAllowance>1234</howMuchEmploymentAndSupportAllowance>
-      howMuchIncapacityBenefit mustBe <howMuchIncapacityBenefit>1234</howMuchIncapacityBenefit>
-      howMuchStatePension mustBe <howMuchStatePension>1234</howMuchStatePension>
+      val otherBenefits = getXpath("otherBenefitsSection", "otherTaxableIncome").\("otherBenefit")
 
       otherBenefits.head mustBe <otherBenefit><name>qwerty</name><amount>12</amount></otherBenefit>
       otherBenefits(1) mustBe <otherBenefit><name>qwerty1</name><amount>34</amount></otherBenefit>
@@ -88,92 +90,75 @@ class RobotsSpec extends  SpecBase with WireMockHelper with MockitoSugar {
 
     "have correct sections in companyBenefitSection" in {
 
-      val nodeSeq: NodeSeq = xmlToNode \\ "companyBenefitsSection"
+      getXpath("companyBenefitsSection", "anyCompanyBenefits") mustBe <anyCompanyBenefits>true</anyCompanyBenefits>
+      getXpath("companyBenefitsSection", "selectCompanyBenefits") mustBe <selectCompanyBenefits>company-car-benefitmedical-benefitfuel-benefitother-company-benefit</selectCompanyBenefits>
+      getXpath("companyBenefitsSection", "howMuchCarBenefits") mustBe <howMuchCarBenefits>1234</howMuchCarBenefits>
+      getXpath("companyBenefitsSection", "howMuchFuelBenefit") mustBe <howMuchFuelBenefit>1234</howMuchFuelBenefit>
+      getXpath("companyBenefitsSection", "howMuchMedicalBenefits") mustBe <howMuchMedicalBenefits>1234</howMuchMedicalBenefits>
 
-      val anyCompanyBenefits = nodeSeq.\("anyCompanyBenefits").head
-      val selectCompanyBenefits = nodeSeq.\("selectCompanyBenefits").head
-      val howMuchCarBenefits = nodeSeq.\("howMuchCarBenefits").head
-      val howMuchFuelBenefit = nodeSeq.\("howMuchFuelBenefit").head
-      val howMuchMedicalBenefits = nodeSeq.\("howMuchMedicalBenefits").head
-      val otherCompanyBenefitsSection = nodeSeq.\("otherCompanyBenefitsSection").\("otherTaxableIncome").\("companyBenefit")
-
+      val otherCompanyBenefitsSection = getXpath("otherCompanyBenefitsSection", "otherTaxableIncome").\("companyBenefit")
 
       otherCompanyBenefitsSection.head mustBe <companyBenefit><name>qwerty</name><amount>12</amount></companyBenefit>
       otherCompanyBenefitsSection(1) mustBe <companyBenefit><name>qwerty1</name><amount>34</amount></companyBenefit>
       otherCompanyBenefitsSection(2) mustBe <companyBenefit><name>qwerty2</name><amount>56</amount></companyBenefit>
 
-
     }
 
     "have correct sections in taxableIncomeSection" in {
 
-      val nodeSeq: NodeSeq = xmlToNode \\ "taxableIncomeSection"
+      getXpath("taxableIncomeSection", "anyTaxableIncome") mustBe <anyTaxableIncome>true</anyTaxableIncome>
 
-      val anyTaxableIncome = nodeSeq.\("anyTaxableIncome").head
+      getXpath("taxableIncomeSection", "rentalIncome").\("howMuch").head mustBe <howMuch>1234</howMuch>
+      getXpath("taxableIncomeSection", "rentalIncome").\("anyTaxPaid").head mustBe <anyTaxPaid>Yes</anyTaxPaid>
+      getXpath("taxableIncomeSection", "rentalIncome").\("taxPaid").head mustBe <taxPaid>123</taxPaid>
 
-      val rentalIncomeAmount = nodeSeq.\("rentalIncome").\("howMuch").head
-      val rentalIncomeAnyTaxPaid = nodeSeq.\("rentalIncome").\("anyTaxPaid").head
-      val rentalIncomeTaxPaidAmount = nodeSeq.\("rentalIncome").\("taxPaid").head
+      getXpath("taxableIncomeSection", "bankInterest").\("howMuch").head mustBe <howMuch>1234</howMuch>
+      getXpath("taxableIncomeSection", "bankInterest").\("anyTaxPaid").head mustBe <anyTaxPaid>Yes</anyTaxPaid>
+      getXpath("taxableIncomeSection", "bankInterest").\("taxPaid").head mustBe <taxPaid>123</taxPaid>
 
-      val bankInterestAmount = nodeSeq.\("bankInterest").\("howMuch").head
-      val bankInterestAnyTaxPaid = nodeSeq.\("bankInterest").\("anyTaxPaid").head
-      val bankInterestTaxPaidAmount = nodeSeq.\("bankInterest").\("taxPaid").head
+      getXpath("taxableIncomeSection", "investmentOrDividend").\("howMuch").head mustBe <howMuch>1234</howMuch>
+      getXpath("taxableIncomeSection", "investmentOrDividend").\("anyTaxPaid").head mustBe <anyTaxPaid>Yes</anyTaxPaid>
+      getXpath("taxableIncomeSection", "investmentOrDividend").\("taxPaid").head mustBe <taxPaid>123</taxPaid>
 
-      val investmentOrDividendIncomeAmount = nodeSeq.\("investmentOrDividend").\("howMuch").head
-      val investmentOrDividendIncomeAnyTaxPaid = nodeSeq.\("investmentOrDividend").\("anyTaxPaid").head
-      val investmentOrDividendIncomeTaxPaidAmount = nodeSeq.\("investmentOrDividend").\("taxPaid").head
-
-      val foreignIncomeIncomeAmount = nodeSeq.\("foreignIncome").\("howMuch").head
-      val foreignIncomeIncomeAnyTaxPaid = nodeSeq.\("foreignIncome").\("anyTaxPaid").head
-      val foreignIncomeIncomeTaxPaidAmount = nodeSeq.\("foreignIncome").\("taxPaid").head
-
-      anyTaxableIncome mustBe <anyTaxableIncome>true</anyTaxableIncome>
-
-      rentalIncomeAmount mustBe <howMuch>1234</howMuch>
-      rentalIncomeAnyTaxPaid mustBe <anyTaxPaid>Yes</anyTaxPaid>
-      rentalIncomeTaxPaidAmount mustBe <taxPaid>123</taxPaid>
-
-      bankInterestAmount mustBe <howMuch>1234</howMuch>
-      bankInterestAnyTaxPaid mustBe <anyTaxPaid>Yes</anyTaxPaid>
-      bankInterestTaxPaidAmount mustBe <taxPaid>123</taxPaid>
-
-      investmentOrDividendIncomeAmount mustBe <howMuch>1234</howMuch>
-      investmentOrDividendIncomeAnyTaxPaid mustBe <anyTaxPaid>Yes</anyTaxPaid>
-      investmentOrDividendIncomeTaxPaidAmount mustBe <taxPaid>123</taxPaid>
-
-      foreignIncomeIncomeAmount mustBe <howMuch>1234</howMuch>
-      foreignIncomeIncomeAnyTaxPaid mustBe <anyTaxPaid>Yes</anyTaxPaid>
-      foreignIncomeIncomeTaxPaidAmount mustBe <taxPaid>123</taxPaid>
+      getXpath("taxableIncomeSection", "foreignIncome").\("howMuch").head mustBe <howMuch>1234</howMuch>
+      getXpath("taxableIncomeSection", "foreignIncome").\("anyTaxPaid").head mustBe <anyTaxPaid>Yes</anyTaxPaid>
+      getXpath("taxableIncomeSection", "foreignIncome").\("taxPaid").head mustBe <taxPaid>123</taxPaid>
 
     }
 
-    "have correct sections in the paymentSection " in {
-      val nodeSeq: NodeSeq = xmlToNode \\ "paymentSection"
+    "have correct sections in the paymentSection when payment address is international" in {
 
-      val whereToSendPayment = nodeSeq.\("whereToSendThePayment").head
-      val nomineeName = nodeSeq.\("nomineeFullname").head
-      val anyAgentRef = nodeSeq.\("anyAgentRef").head
-      val agentRef = nodeSeq.\("agentReference").head
-      val addressInUK = nodeSeq.\("isPaymentAddressInTheUK").head
-      val internationalAddress = nodeSeq.\("paymentAddress").head
+      getXpath("paymentSection", "whereToSendThePayment") mustBe <whereToSendThePayment>nominee</whereToSendThePayment>
+      getXpath("paymentSection", "nomineeFullname") mustBe <nomineeFullname>Nominee</nomineeFullname>
+      getXpath("paymentSection", "anyAgentRef") mustBe <anyAgentRef>Yes</anyAgentRef>
+      getXpath("paymentSection", "agentReference") mustBe <agentReference>12341234</agentReference>
+      getXpath("paymentSection", "isPaymentAddressInTheUK") mustBe <isPaymentAddressInTheUK>false</isPaymentAddressInTheUK>
+      getXpath("paymentSection", "paymentAddress") mustBe <paymentAddress><internationalAddress>1,2,Country</internationalAddress></paymentAddress>
+    }
 
-      whereToSendPayment mustBe <whereToSendThePayment>nominee</whereToSendThePayment>
-      nomineeName mustBe <nomineeFullname>Nominee</nomineeFullname>
-      anyAgentRef mustBe <anyAgentRef>Yes</anyAgentRef>
-      agentRef mustBe <agentReference>12341234</agentReference>
-      addressInUK mustBe <isPaymentAddressInTheUK>false</isPaymentAddressInTheUK>
-      internationalAddress mustBe <paymentAddress><internationalAddress>1,2,Country</internationalAddress></paymentAddress>
+    "have correct sections in the paymentSection when payment address is in the UK" in {
+
+      val fullUserAnswers: UserAnswers = MockUserAnswers.fullValidUserAnswers
+
+      when(fullUserAnswers.isPaymentAddressInTheUK) thenReturn Some(true)
+      when(fullUserAnswers.paymentInternationalAddress) thenReturn None
+      when(fullUserAnswers.paymentUKAddress) thenReturn Some(UkAddress("qwerty", "qwerty1", None, None, None, "AB1 0CD"))
+
+      val fullXml: XmlFormat.Appendable = robots(fullUserAnswers, new RobotsXmlHelper(fullUserAnswers)(messages))
+      val newXmlToNode: Elem = loadString(fullXml.toString.replaceAll("\\s", ""))
+
+      getXpath("paymentSection", "whereToSendThePayment", newXmlToNode) mustBe <whereToSendThePayment>nominee</whereToSendThePayment>
+      getXpath("paymentSection", "nomineeFullname", newXmlToNode) mustBe <nomineeFullname>Nominee</nomineeFullname>
+      getXpath("paymentSection", "anyAgentRef", newXmlToNode) mustBe <anyAgentRef>Yes</anyAgentRef>
+      getXpath("paymentSection", "agentReference", newXmlToNode) mustBe <agentReference>12341234</agentReference>
+      getXpath("paymentSection", "isPaymentAddressInTheUK", newXmlToNode) mustBe <isPaymentAddressInTheUK>true</isPaymentAddressInTheUK>
+      getXpath("paymentSection", "paymentAddress", newXmlToNode) mustBe <paymentAddress><ukAddress>qwerty,qwerty1,AB10CD</ukAddress></paymentAddress>
     }
 
     "have the correct parts in contact section" in {
-      val nodeSeq: NodeSeq = xmlToNode \\ "contactSection"
 
-      val telephoneOption = nodeSeq.\("anyTelephoneNumber").head
-      val telephoneNumber = nodeSeq.\("telephoneNumber").head
-
-      telephoneOption mustBe  <anyTelephoneNumber>Yes</anyTelephoneNumber>
-      telephoneNumber mustBe <telephoneNumber>0191123123</telephoneNumber>
-
+      getXpath("contactSection", "anyTelephoneNumber") mustBe  <anyTelephoneNumber>Yes</anyTelephoneNumber>
+      getXpath("contactSection", "telephoneNumber") mustBe <telephoneNumber>0191123123</telephoneNumber>
     }
   }
 }
