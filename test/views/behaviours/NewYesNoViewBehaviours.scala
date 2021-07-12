a6697f09e4dd142d07f0f8cc3ce0c1af29bd2235
stephen-oppong-beduh <56881842+stephen-oppong-beduh@users.noreply.github.com>
2021-07-12 11:14:07 +0100

    DL-5917 - anyBenefits.scala.html to use play-frontend components

---
+++ test/views/behaviours/NewYesNoViewBehaviours.scala
@@ -0,0 +1,119 @@
+/*
+ * Copyright 2021 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package views.behaviours
+
+import forms.BooleanForm
+import play.api.data.Form
+import play.twirl.api.HtmlFormat
+
+trait NewYesNoViewBehaviours extends NewQuestionViewBehaviours[Boolean] {
+
+  val formProvider = new BooleanForm()
+  val form = formProvider()
+
+  def yesNoPage(createView: (Form[Boolean]) => HtmlFormat.Appendable,
+                messageKeyPrefix: String,
+                expectedFormAction: String,
+                expectedHintTextKey: Option[String],
+                args: Any*) = {
+
+    "behave like a page with a Yes/No question" when {
+      "rendered" must {
+        "contain a legend for the question" in {
+          val doc = asDocument(createView(form))
+          val legends = doc.getElementsByTag("legend")
+          legends.size mustBe 1
+        }
+
+        "contain a heading" in {
+          val doc = asDocument(createView(form))
+          assertContainsText(doc, messages(s"$messageKeyPrefix.heading", args: _*))
+        }
+
+        if(expectedHintTextKey.isDefined){
+          "render a hint" in {
+            val doc = asDocument(createView(form))
+            assertYesNoHint(doc, expectedHintTextKey)
+          }
+        } else {
+          "not render a hint" in {
+            val doc = asDocument(createView(form))
+            assertNotRenderedByCssSelector(doc, ".form-hint")
+          }
+        }
+
+        "contain an input for the value" in {
+          val doc = asDocument(createView(form))
+          assertRenderedById(doc, "value")
+          assertRenderedById(doc, "value-2")
+        }
+
+        "have no values checked when rendered with no form" in {
+          val doc = asDocument(createView(form))
+          assert(!doc.getElementById("value").hasAttr("checked"))
+          assert(!doc.getElementById("value-2").hasAttr("checked"))
+        }
+
+        "not render an error summary" in {
+          val doc = asDocument(createView(form))
+          assertNotRenderedById(doc, "error-summary_header")
+        }
+
+				"show error in the title" in {
+					val doc = asDocument(createView(form.withError(error)))
+					doc.title.contains("Error: ") mustBe true
+				}
+      }
+
+      "rendered with a value of true" must {
+        behave like answeredYesNoPage(createView, true)
+      }
+
+      "rendered with a value of false" must {
+        behave like answeredYesNoPage(createView, false)
+      }
+
+      "rendered with an error" must {
+        "show an error summary" in {
+          val doc = asDocument(createView(form.withError(error)))
+          assertRenderedById(doc, "error-summary-title")
+        }
+
+        "show an error in the value field's label" in {
+          val doc = asDocument(createView(form.withError(error)))
+          val errorSpan = doc.getElementsByClass("govuk-error-message").first
+          errorSpan.text must include(messages(errorMessage))
+        }
+      }
+    }
+  }
+
+
+  def answeredYesNoPage(createView: (Form[Boolean]) => HtmlFormat.Appendable, answer: Boolean) = {
+
+    "have only the correct value checked" in {
+      val doc = asDocument(createView(form.fill(answer)))
+      assert(doc.getElementById("value").hasAttr("checked") == answer)
+      assert(doc.getElementById("value-2").hasAttr("checked") != answer)
+    }
+
+    "not render an error summary" in {
+      val doc = asDocument(createView(form.fill(answer)))
+      assertNotRenderedById(doc, "error-summary_header")
+    }
+  }
+}
