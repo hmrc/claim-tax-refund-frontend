89a196ae1c4bb831063591569845e5456b93ae88
danny-waughman <daniel.waughman@digital.hmrc.gov.uk>
2024-07-02 13:43:30 +0100

    DL-14111 Correcting format for Mongo TTL

--- app/repositories/SessionRepository.scala
+++ app/repositories/SessionRepository.scala
@@ -17,20 +17,24 @@
 package repositories
 
 import models.CacheMap
+import org.apache.pekko.actor.ActorSystem
+import org.bson.BsonType
 
 import java.time.Instant
 import javax.inject.{Inject, Singleton}
-import org.mongodb.scala.model.{IndexModel, IndexOptions, UpdateOptions}
+import org.mongodb.scala.model.{IndexModel, IndexOptions, ReplaceOptions, UpdateOptions}
 import org.mongodb.scala.model.Filters._
 import org.mongodb.scala.model.Indexes._
 import org.mongodb.scala.model.Updates._
-import play.api.Configuration
-import play.api.libs.json.{JsValue, Json, OFormat}
+import play.api.{Configuration, Logging}
+import play.api.libs.json.{Format, JsValue, Json, OFormat}
 import uk.gov.hmrc.mongo.MongoComponent
+import uk.gov.hmrc.mongo.play.json.formats.MongoJavatimeFormats
 import uk.gov.hmrc.mongo.play.json.{Codecs, PlayMongoRepository}
 
+import java.util.concurrent.TimeUnit
 import scala.concurrent.{ExecutionContext, Future}
-import scala.concurrent.duration.SECONDS
+import scala.concurrent.duration.{FiniteDuration, SECONDS}
 
 
 case class DatedCacheMap(id: String,
@@ -38,12 +42,13 @@ case class DatedCacheMap(id: String,
                          lastUpdated: Instant = Instant.now())
 
 object DatedCacheMap {
+  implicit val dateFormat: Format[Instant] = MongoJavatimeFormats.Implicits.jatInstantFormat
   implicit val formats: OFormat[DatedCacheMap] = Json.format[DatedCacheMap]
 
   def apply(cacheMap: CacheMap): DatedCacheMap = DatedCacheMap(cacheMap.id, cacheMap.data)
 }
 
-class ReactiveMongoRepository(config: Configuration, mongo: MongoComponent)(implicit executionContext: ExecutionContext)
+class ReactiveMongoRepository(config: Configuration, mongo: MongoComponent)(implicit ec: ExecutionContext)
   extends PlayMongoRepository[DatedCacheMap](
     collectionName = config.get[String]("appName"),
     mongoComponent = mongo,
@@ -54,21 +59,32 @@ class ReactiveMongoRepository(config: Configuration, mongo: MongoComponent)(impl
         .expireAfter(config.get[Long]("mongodb.timeToLiveInSeconds"), SECONDS))
     )
     , extraCodecs = Seq(Codecs.playFormatCodec(CacheMap.formats))
-  ) {
+  ) with Logging {
 
   def upsert(cm: CacheMap): Future[Boolean] = {
     val dcm = DatedCacheMap(cm)
-    collection.updateOne(
+    collection.replaceOne(
       filter = equal("id", dcm.id),
-      update = combine(
-        set("data", Codecs.toBson(dcm.data)),
-        set("lastUpdated", Codecs.toBson(dcm.lastUpdated))),
-      UpdateOptions().upsert(true)
+      replacement = dcm,
+      ReplaceOptions().upsert(true)
     ).toFuture().map(_.wasAcknowledged())
   }
+
   def get(id: String): Future[Option[CacheMap]] =
     collection.find[CacheMap](and(equal("id", id))).headOption()
 
+  def resetLastUpdated(): Future[Long] = {
+    collection.updateMany(`type`("lastUpdated", BsonType.STRING), set("lastUpdated", Instant.now()))
+      .toFuture().map(_.getModifiedCount)
+  }
+
+  val actorSystem = ActorSystem()
+  actorSystem.scheduler.scheduleOnce(FiniteDuration(10, TimeUnit.SECONDS)) {
+    resetLastUpdated() map { n =>
+      logger.warn(s"Updated $n documents with a new lastUpdated timestamp")
+    }
+    actorSystem.terminate()
+  }
 }
 
 @Singleton
